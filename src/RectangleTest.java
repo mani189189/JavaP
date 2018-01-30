f782041400248ef31827f19ad80c1ad8fd3a3ee6
mani <koppisettimanikanta7@gmail.com>
2018-01-31 04:58:16 +0530

    save

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -5,16 +5,6 @@ public class RectangleTest {
   Rectangle myRectangle = new Rectangle(5,6);
 
   @Test
-  public void testGetArea() {
-    assertEquals(myRectangle.getArea(), 30);
-  }
-
-  @Test
-  public void testGetPerimeter() {
-    assertEquals(myRectangle.getPerimeter(), 22);
-  }
-
-  @Test
   public void testLength() {
     assertEquals(myRectangle.length, 5);
   }
@@ -22,5 +12,15 @@ public class RectangleTest {
   @Test
   public void testWidth() {
     assertEquals(myRectangle.width, 6);
+  }
+
+  @Test
+  public void testGetArea() {
+    assertEquals(myRectangle.getArea(), 30);
+  }
+
+  @Test
+  public void testGetPerimeter() {
+    assertEquals(myRectangle.getPerimeter(), 22);
   }
 }
